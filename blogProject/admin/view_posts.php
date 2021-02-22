bc2f5eb4cfde3391b3df3b5b93d634aab5f3b6df
Richard <richard.santarinareyes@medieinstitutet.se>
2021-02-22 11:15:53 +0100

    all posts showing

--- blogProject/admin/view_posts.php
+++ blogProject/admin/view_posts.php
@@ -18,7 +18,7 @@
                           <th>Id</th>
                           <th>Author</th>
                           <th>Title</th>
-                          <th>Category</th>
+                          <th>Menu</th>
                           <th>Status</th>
                           <th>Image</th>
                           <th>Tags</th>
@@ -27,17 +27,35 @@
                       </tr>
                   </thead>
                   <tbody>
+                  <?php 
+                  $getPosts = $abc->query("SELECT * FROM posts");
+                  
+                  while ($postsrow = $getPosts->fetch()) {
+                      $vp_id = $postsrow["post_id"];
+                      $vp_author = $postsrow["post_author"];
+                      $vp_title = $postsrow["post_title"];
+                      //   $vp_menu = $postsrow["post_title"];
+                      $vp_status = $postsrow["post_status"];
+                      $vp_img = $postsrow["post_img"];
+                      $vp_tags = $postsrow["post_tags"];
+                      $vp_comments = $postsrow["post_comment_count"];
+                      $vp_date = $postsrow["post_date"];
+                      
+                      echo "
                       <tr>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                          <td>Placeholder</td>
-                      </tr>
+                        <td> $vp_id</td>
+                        <td> $vp_author</td>
+                        <td> $vp_title</td>
+                        <td>Placeholder</td>
+                        <td> $vp_status</td>
+                        <td> $vp_img</td>
+                        <td> $vp_tags</td>
+                        <td> $vp_comments</td>
+                        <td> $vp_date</td>
+                        </tr>
+                        ";
+                  }
+                  ?>
                   </tbody>
               </table>
             </div>
